70e904e707b7d4e020b2df6d94ec6e11c01cc503
Alfredo Lorentiar Santonanda <alorentiar03@gmail.com>
2023-04-29 22:27:48 +0700

    update

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	// "fmt"
 	"log"
-	"net/http"
 
 	"bwastartup/user"
+	"bwastartup/handler"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
@@ -13,37 +13,6 @@ import (
 )
 
 func main() {
-	// dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn),&gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection Established")
-
-	// var users []user.User
-	// length := len(users)
-
-	// fmt.Println("Total Rows: ", length)
-
-	// db.Find(&users)
-
-	// length = len(users)
-	// fmt.Println("Total Rows: ", length)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("================================")
-	// }
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-} 
-
-//handler dari db
-func handler (c *gin.Context){
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn),&gorm.Config{})
 
@@ -51,13 +20,17 @@ func handler (c *gin.Context){
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
 
-	c.JSON(http.StatusOK,users)
+	userHandler := handler.NewUserHandler(userService)
 
-	// handler
-	// service
-	// repository
-	// entity
-}
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users",userHandler.RegisterUser)
+
+	router.Run()
+
+	
+} 
